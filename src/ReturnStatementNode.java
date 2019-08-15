d3091eff033237d0847a50a64831f837c48b9e5c
Patrick Norton <patrick.147.norton@gmail.com>
2019-08-15 06:09:41 -0400

    Added conditionals to continue, break, and return statements

--- src/ReturnStatementNode.java
+++ src/ReturnStatementNode.java
@@ -1,11 +1,18 @@
 public class ReturnStatementNode implements SimpleFlowNode {
     private TestNode[] returned;
+    private TestNode cond;
 
-    public ReturnStatementNode(TestNode[] returned) {
+    public ReturnStatementNode(TestNode[] returned, TestNode cond) {
         this.returned = returned;
+        this.cond = cond;
     }
 
     public TestNode[] getReturned() {
         return returned;
+    }
+
+    @Override
+    public TestNode getCond() {
+        return cond;
     }
 }
