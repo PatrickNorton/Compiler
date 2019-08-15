97f7320f4768e2cc5759b2d21c51c44debd51e63
Patrick Norton <patrick.147.norton@gmail.com>
2019-08-15 06:09:41 -0400

    Added conditionals to continue, break, and return statements

--- src/ContinueStatementNode.java
+++ src/ContinueStatementNode.java
@@ -1,2 +1,11 @@
 public class ContinueStatementNode implements SimpleFlowNode {
+    private TestNode cond;
+
+    public ContinueStatementNode(TestNode cond) {
+        this.cond = cond;
+    }
+
+    public TestNode getCond() {
+        return cond;
+    }
 }
--- src/SimpleFlowNode.java
+++ src/SimpleFlowNode.java
@@ -1,2 +1,3 @@
 public interface SimpleFlowNode extends SimpleStatementNode {
+    TestNode getCond();
 }
