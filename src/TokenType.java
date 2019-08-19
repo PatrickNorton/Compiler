d92a8b31bd4e5a5eb5bcf0d671e4a858878d4eb9
Patrick Norton <patrick.147.norton@gmail.com>
2019-08-19 10:31:19 -0400

    Turned token types into an enum

---
+++ src/TokenType.java
@@ -0,0 +1,61 @@
+import org.intellij.lang.annotations.Language;
+
+import java.util.regex.Pattern;
+
+public enum TokenType {
+    // TODO? Turn dot into its own token
+    // Whitespace. Matches comments, spaces, and escaped newlines
+    WHITESPACE("^(#\\|((?!\\|#).|\n)*\\|#|#.*| +|\\\\\n)"),
+    // Matches when input is empty
+    EPSILON("^\\z"),
+    // Matches newlines of all types
+    NEWLINE("^\\R"),
+    // Matches list of descriptor words (const, private, etc.)
+    DESCRIPTOR("^\\b(private|const|final|pubget|static)\\b"),
+    // Matches keywords (if, else, class, do, etc.)
+    KEYWORD("^\\b(if|for|else|do|func|class|method|while|in|from|(im|ex)port"
+            +"|typeget|dotimes|break|continue|return|context|get|set|lambda"
+            +"|property|enter|exit|try|except|finally|with|as|assert|del|yield"
+            +"|raise|typedef|some|interface|cast(ed)?|to)\\b"),
+    // Matches self.foo() or cls.foo()
+    // TODO? Remove this and just have as standard keywords
+    SELF_CLS("^\\b(self|cls)(\\.[_a-zA-Z][_a-zA-Z0-9.]*)?\\b"),
+    // Matches open braces
+    OPEN_BRACE("^[\\[({]"),
+    // Matches close braces
+    CLOSE_BRACE("^[])}]"),
+    // The comma in between list items
+    COMMA("^,"),
+    // Augmented assignment, e.g. +=
+    // Parsed separately from standard operators because of their different use
+    AUG_ASSIGN("^([+\\-%]|([*/])\\2?|<<|>>|[&|^~])="),
+    // Standard, boring operators, + - **
+    OPERATOR("^(->|==|!=|[><]=?|([+\\-*/])\\2?|<<|>>|[&|^~%])"),
+    // Assignment, and dynamic assignment (:=)
+    ASSIGN("^:?="),
+    // String literals, including f-strings
+    STRING("^[rfb]*\"([^\"]|\\\\\"|\n)+(?<!\\\\)\""),
+    // Boolean operators
+    BOOL_OP("^\\b(and|or|not|xor)\\b"),
+    // Numbers, from 123 to 0xab4f6.245
+    NUMBER("^(0x[0-9a-f]+(\\.[0-9a-f]+)?|(0[ob])?[0-9]+(\\.[0-9]+)?)\\b"),
+    // That special operator definition syntax
+    OPERATOR_SP("^\\b(operator *(r?(==|!=|([+\\-*/])\\4?|[><]=?)|\\[]=?|\\(\\)"
+            + "|u-|iter|new|in|missing|del|str|repr|bool|del(\\[])?|<<|>>|[&|^~%]))"),
+    // Dotted variables (e.g. [abc()].foo())
+    DOTTED_VAR("^(?!operator\\b)\\.[_a-zA-Z][_a-zA-Z0-9]*(\\.[_a-zA-Z][_a-zA-Z0-9]*)*\\b"),
+    // Regular variables
+    VARIABLE("^(?!operator\\b)[_a-zA-Z][_a-zA-Z0-9]*(\\.[_a-zA-Z][_a-zA-Z0-9]*)*\\b"),
+    // operator functions, like \+
+    OP_FUNC("^\\\\(==|!=|[><]=?|r?[+\\-*/]{1,2}|u-|<<|>>|[&|^~%])"),
+    // Colons, for slicing syntax etc.
+    COLON("^:"),
+    // The ellipsis unicorn
+    ELLIPSIS("^\\.{3}");
+
+    final Pattern regex;
+
+    TokenType(@Language("RegExp") String regex) {
+        this.regex = Pattern.compile(regex);
+    }
+}
--- src/Tokenizer.java
+++ src/Tokenizer.java
@@ -1,35 +1,12 @@
-import javax.management.relation.RoleUnresolved;
 import java.util.LinkedList;
 import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 
-// TODO? Remove self_cls tokens
 public class Tokenizer {
-    public class TokenInfo {
-        public final Pattern regex;
-        public final int token;
-
-        public TokenInfo(Pattern regex, int token) {
-            super();
-            this.regex = regex;
-            this.token = token;
-        }
-    }
-
-
-    private LinkedList<TokenInfo> tokenInfos;
     private LinkedList<Token> tokens;
 
     public Tokenizer() {
-        tokenInfos = new LinkedList<>();
         tokens = new LinkedList<>();
-    }
-
-    public void add(Pattern regex, int token) {
-        tokenInfos.add(
-                new TokenInfo(regex, token)
-        );
     }
 
     public void tokenize(String str) {
@@ -37,13 +14,13 @@ public class Tokenizer {
         tokens.clear();
         while (!s.equals("")) {
             boolean match = false;
-            for (TokenInfo info : tokenInfos) {
+            for (TokenType info : TokenType.values()) {
                 Matcher m = info.regex.matcher(s);
                 if (m.find()) {
                     match = true;
-                    if (info.token != 0) {
+                    if (info != TokenType.WHITESPACE) {
                         String tok = m.group();
-                        tokens.add(new Token(info.token, tok));
+                        tokens.add(new Token(info, tok));
                     }
                     s = m.replaceFirst("");
                     break;
@@ -59,58 +36,6 @@ public class Tokenizer {
 
     public static Tokenizer parse(String str) {
         Tokenizer tokenizer = new Tokenizer();
-        // Comments and whitespace
-        tokenizer.add(Pattern.compile("^(#\\|((?!\\|#).|\\n)*\\|#|#.*| +|\\\\\\n)"), 0);
-        // Newlines
-        tokenizer.add(Pattern.compile("^\\R"), 1);
-        // Descriptors, e.g. private, etc.
-        tokenizer.add(Pattern.compile("^\\b(private|const|final|pubget|static)\\b"), 2);
-        // Other keywords, such as for control flow or typeget
-        tokenizer.add(Pattern.compile("^\\b(if|for|else|do|func|class|method|while|in|from|(im|ex)port"
-                               +"|typeget|dotimes|break|continue|return|context|get|set|lambda"
-                               +"|property|enter|exit|try|except|finally|with|as|assert|del|yield"
-                               +"|raise|typedef|some|interface|cast(ed)?|to)\\b"), 3);
-        // The self and cls keywords
-        // TODO? Move this into variable section
-        tokenizer.add(Pattern.compile("^\\b(self|cls)(\\.[_a-zA-Z][_a-zA-Z0-9.]*)?\\b"), 4);
-        // Opening braces
-        tokenizer.add(Pattern.compile("^[\\[({]"), 5);
-        // Closing braces
-        tokenizer.add(Pattern.compile("^[])}]"), 6);
-        // The comma
-        tokenizer.add(Pattern.compile("^,"), 7);
-        // Augmented assignment operators
-        // These are separate from other operators because they act differently,
-        // and thus need to be parsed separately
-        tokenizer.add(Pattern.compile("^([+\\-%]|([*/])\\2?|<<|>>|[&|^~])="), 8);
-        // Normal operators
-        // Operators in their natural habitat, not masquerading as anything else
-        // TODO? Move arrow operator to its own section
-        tokenizer.add(Pattern.compile("^(->|==|!=|[><]=?|([+\\-*/])\\2?|<<|>>|[&|^~%])"), 9);
-        // Assignment operators
-        tokenizer.add(Pattern.compile("^:?="), 10);
-        // String literals
-        // These are token-ed separately, so they don't mess with the syntax of everything else
-        tokenizer.add(Pattern.compile("^[rfb]?\"([^\"]|\\\\\"|\n)+(?<!\\\\)\""), 11);
-        // Boolean operators
-        tokenizer.add(Pattern.compile("^\\b(and|or|not|xor)\\b"), 12);
-        // Digits, incl. those in other bases
-        tokenizer.add(Pattern.compile("^(0x[0-9a-f]+(\\.[0-9a-f]+)?|(0[ob])?[0-9]+(\\.[0-9]+)?)\\b"), 13);
-        // The crazy operator syntax
-        tokenizer.add(Pattern.compile("^\\b(operator *(r?(==|!=|([+\\-*/])\\4?|[><]=?)|\\[]=?|\\(\\)" +
-                             "|u-|iter|new|in|missing|del|str|repr|bool|del(\\[])?|<<|>>|[&|^~%]))"), 14);
-        // Dotted variable names
-        tokenizer.add(Pattern.compile("^(?!operator\\b)\\.[_a-zA-Z][_a-zA-Z0-9]*(\\.[_a-zA-Z][_a-zA-Z0-9]*)*\\b"), 19);
-        // Variable names
-        // Includes a check to make sure operator never shows up as a variable,
-        // because it is a keyword, even though it doesn't show up in the keyword check
-        tokenizer.add(Pattern.compile("^\\b(?!operator\\b)[_a-zA-Z][_a-zA-Z0-9]*(\\.[_a-zA-Z][_a-zA-Z0-9]+)*\\b"), 15);
-        // Operator-functions
-        tokenizer.add(Pattern.compile("^\\\\(==|!=|[><]=?|r?[+\\-*/]{1,2}|u-|<<|>>|[&|^~%])"), 16);
-        // Colons, for slice syntax and others
-        tokenizer.add(Pattern.compile("^:"), 17);
-        // The ellipsis
-        tokenizer.add(Pattern.compile("^\\.{3}"), 18);
         try {
             tokenizer.tokenize(str);
         } catch (RuntimeException e) {
