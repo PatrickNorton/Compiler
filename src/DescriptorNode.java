6444632ae913b722b060dfb78b924428a31896ea
Patrick Norton <patrick.147.norton@gmail.com>
2019-08-30 12:55:30 -0400

    More class documentation

--- src/DescriptorNode.java
+++ src/DescriptorNode.java
@@ -1,6 +1,23 @@
+import org.jetbrains.annotations.Contract;
+
+// TODO: Make me an enum
+
+/**
+ * The node representing all descriptors.
+ * <p>
+ *     This node is used for the descriptor types in such things as {@link
+ *     ClassStatementNode} and {@link InterfaceStatementNode}, which are the
+ *     only statements which may be preceded by descriptors.
+ * </p>
+ *
+ * @author Patrick Norton
+ * @see InterfaceStatementNode
+ * @see ClassStatementNode
+ */
 public class DescriptorNode implements AtomicNode {
     public String name;
 
+    @Contract(pure = true)
     public DescriptorNode(String name) {
         this.name = name;
     }
