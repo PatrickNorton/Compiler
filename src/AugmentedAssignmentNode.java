b352b050ba78d200c0418228236b518f206e27d4
Patrick Norton <patrick.147.norton@gmail.com>
2019-08-30 05:55:59 -0400

    Added more documentation

--- src/AugmentedAssignmentNode.java
+++ src/AugmentedAssignmentNode.java
@@ -1,8 +1,25 @@
+import org.jetbrains.annotations.Contract;
+
+/**
+ * The class representing an augmented assignment, such as +=, **=, &=, etc.
+ * <p>
+ *     This class also has no parse static method, as it is instead parsed by
+ *     {@link BaseNode} in a private method, as that is where it is easiest to
+ *     handle.
+ * </p>
+ */
 public class AugmentedAssignmentNode implements AssignStatementNode {
     private OperatorNode operator;
     private NameNode name;
     private TestNode value;
 
+    /**
+     * Create new AugmentedAssignmentNode.
+     * @param operator the operator being augmented
+     * @param name the name which is augmented
+     * @param value the value to which it is being augmented and then assigned
+     */
+    @Contract(pure = true)
     public AugmentedAssignmentNode(OperatorNode operator, NameNode name, TestNode value) {
         this.operator = operator;
         this.name = name;
@@ -13,6 +30,7 @@ public class AugmentedAssignmentNode implements AssignStatementNode {
         return operator;
     }
 
+    @Override
     public NameNode[] getName() {
         return new NameNode[] {name};
     }
--- src/ClassBodyNode.java
+++ src/ClassBodyNode.java
@@ -1,18 +1,42 @@
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
 import java.util.ArrayList;
 
+/**
+ * The class representing the body of a class.
+ *
+ * @author Patrick Norton
+ * @see StatementBodyNode
+ */
 public class ClassBodyNode extends StatementBodyNode {
     private ClassStatementNode[] statements;
 
     public ClassBodyNode(ClassStatementNode... statements) {
-        // super(statements);
         this.statements = statements;
     }
 
+    @Override
     public ClassStatementNode[] getStatements() {
         return statements;
     }
 
-    static ClassBodyNode parse(TokenList tokens) {
+    /**
+     * Parse ClassBodyNode from list of tokens.
+     * <p>
+     *     A class body is simply a list of class statements, e.g. statements
+     *     which can have a declaration in front of them. The statement must
+     *     also be surrounded by curly braces, not doing so will result in a
+     *     ParserException being raised. The syntax of a ClassStatementNode is
+     *     <code> "{" *{@link ClassStatementNode} "}" NEWLINE</code>.
+     * </p>
+     * @param tokens The list of tokens passed
+     * @return The parsed ClassBodyNode
+     */
+    @NotNull
+    @Contract("_ -> new")
+    static ClassBodyNode parse(@NotNull TokenList tokens) {
+        // FIXME: Allow lack of newlines around braces
         if (!tokens.tokenIs("{")) {
             throw new ParserException("The body of a class must be enclosed in curly brackets");
         }
