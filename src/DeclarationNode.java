ad140f7381a20f6f3b06b187ed2b83d4c72f5700
Patrick Norton <patrick.147.norton@gmail.com>
2019-08-30 11:33:16 -0400

    Began documenting classes

--- src/DeclarationNode.java
+++ src/DeclarationNode.java
@@ -1,8 +1,23 @@
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * The class representing a variable declaration without assignment.
+ *
+ * @author Patrick Norton
+ * @see DeclaredAssignmentNode
+ */
 public class DeclarationNode implements AssignStatementNode, ClassStatementNode {
     private TypeNode type;
     private VariableNode name;
     private DescriptorNode[] descriptors;
 
+    /**
+     * Create a new instance of DeclarationNode.
+     * @param type The type of the declared variable
+     * @param name The name of that variable
+     */
+    @Contract(pure = true)
     public DeclarationNode(TypeNode type, VariableNode name) {
         this.type = type;
         this.name = name;
@@ -12,6 +27,7 @@ public class DeclarationNode implements AssignStatementNode, ClassStatementNode 
         return type;
     }
 
+    @Override
     public NameNode[] getName() {
         return new NameNode[] {name};
     }
@@ -25,6 +41,19 @@ public class DeclarationNode implements AssignStatementNode, ClassStatementNode 
         this.descriptors = nodes;
     }
 
+    /**
+     * Parse a DeclarationNode from a list of tokens.
+     * <p>
+     *     The syntax for a DeclarationNode is: <code>{@link TypeNode}
+     *     {@link VariableNode}</code>. It is important that there be a check
+     *     done previously to determine that this is not simply part of a
+     *     larger node, such as a {@link DeclaredAssignmentNode}, or other
+     *     such thing.
+     * </p>
+     * @param tokens The list of tokens to be destructively parsed
+     * @return The newly parsed DeclarationNode
+     */
+    @NotNull
     static DeclarationNode parse(TokenList tokens) {
         TypeNode type = TypeNode.parse(tokens);
         VariableNode var = VariableNode.parse(tokens);
