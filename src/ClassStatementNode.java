6719fe51e01a3bdfca5db854ac065ba3be5e8ed7
Patrick Norton <patrick.147.norton@gmail.com>
2019-08-30 05:55:59 -0400

    Added more documentation

--- src/ClassStatementNode.java
+++ src/ClassStatementNode.java
@@ -1,7 +1,35 @@
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
 import java.util.ArrayList;
 
+/**
+ * The interface for statements which are valid in a class body.
+ * <p>
+ *     This inherits the method {@code addDescriptor} from
+ *     InterfaceStatementNode, this allows all statements in a class body to
+ *     add descriptors to themselves
+ * </p>
+ *
+ * @author Patrick Norton
+ * @see InterfaceStatementNode
+ */
 public interface ClassStatementNode extends InterfaceStatementNode {
-    static ClassStatementNode parse(TokenList tokens) {
+    /**
+     * Parse a ClassStatementNode from a list of tokens.
+     * <p>
+     *     ClassStatementNodes are simply any {@link BaseNode} which inherits
+     *     from this node, and that is how they are parsed, with a call to
+     *     {@link BaseNode#parse}, and a typecast filter. This node
+     *     is defined as the union of all its subclasses, check an inheritance
+     *     tree or grep for those.
+     * </p>
+     * @param tokens Tokens to be parsed; parse does operate destructively on
+     *               them
+     * @return The newly parsed ClassStatementNode
+     */
+    @NotNull
+    static ClassStatementNode parse(@NotNull TokenList tokens) {
         if (tokens.tokenIs("static") && tokens.getToken(1).is("{")) {
             tokens.nextToken();
             return new StaticBlockNode(StatementBodyNode.parse(tokens));  // TODO? StaticBlockNode.parse
@@ -13,7 +41,20 @@ public interface ClassStatementNode extends InterfaceStatementNode {
         throw new ParserException(tokens.getFirst()+" is not a valid class statement");
     }
 
-    static ClassStatementNode parseDescriptor(TokenList tokens) {
+    /**
+     * Parse a list of tokens for anything starting with a descriptor.
+     * <p>
+     *     This delegates to three helper methods based on the next token in
+     *     the list. The first token in the list must be a descriptor, and not
+     *     anything else. The token after all descriptors must be either a
+     *     keyword, special operator, or a name. All of these must then form
+     *     a valid ClassStatementNode.
+     * </p>
+     * @param tokens The tokens to be parsed destructively
+     * @return The newly parsed ClassStatementNode
+     */
+    @NotNull
+    static ClassStatementNode parseDescriptor(@NotNull TokenList tokens) {
         ArrayList<DescriptorNode> descriptors = new ArrayList<>();
         if (tokens.tokenIs("class")) {
             descriptors.add(new DescriptorNode("class"));
@@ -23,6 +64,7 @@ public interface ClassStatementNode extends InterfaceStatementNode {
             descriptors.add(new DescriptorNode(tokens.getFirst().sequence));
             tokens.nextToken();
         }
+        // FIXME: Assert for the fact that there was at least one keyword
         switch (tokens.getFirst().token) {
             case KEYWORD:
                 return parseDescriptorKeyword(tokens, descriptors);
@@ -35,7 +77,22 @@ public interface ClassStatementNode extends InterfaceStatementNode {
         }
     }
 
-    private static ClassStatementNode parseDescriptorKeyword(TokenList tokens, ArrayList<DescriptorNode> descriptors) {
+    /**
+     * Parse a statement beginning with a keyword, which has been preceded by
+     * one or more descriptors.
+     * <p>
+     *     There are three keyword-beginning items which may be preceded by a
+     *     descriptor. These are {@link ClassDefinitionNode}, {@link
+     *     MethodDefinitionNode}, and {@link InterfaceDefinitionNode}. All of
+     *     these have descriptors added to them at the end of the
+     * </p>
+     * @param tokens The list of tokens to be destructively parsed
+     * @param descriptors The list of descriptors which goes in front of them
+     * @return The descriptor-added node
+     */
+    @NotNull
+    @Contract("_, _ -> new")
+    private static ClassStatementNode parseDescriptorKeyword(@NotNull TokenList tokens, @NotNull ArrayList<DescriptorNode> descriptors) {
         assert tokens.tokenIs(TokenType.KEYWORD);
         ClassStatementNode node;
         switch (tokens.getFirst().sequence) {
@@ -51,6 +108,7 @@ public interface ClassStatementNode extends InterfaceStatementNode {
             case "func":
             case "if":
             case "for":
+            case "elif":
             case "else":
             case "do":
             case "dotimes":
@@ -71,14 +129,39 @@ public interface ClassStatementNode extends InterfaceStatementNode {
         return node;
     }
 
-    private static ClassStatementNode parseDescriptorOpSp(TokenList tokens, ArrayList<DescriptorNode> descriptors) {
+    /**
+     * Parses a special operator preceded by a descriptor.
+     * <p>
+     *     This is simply a delegator method to {@link
+     *     OperatorDefinitionNode#parse(TokenList)}, with the caveat that it
+     *     adds on the parsed descriptors at the end.
+     * </p>
+     * @param tokens List of tokens to be parsed destructively
+     * @param descriptors List of descriptors to be added on
+     * @return The descriptor-added node
+     */
+    @NotNull
+    private static ClassStatementNode parseDescriptorOpSp(@NotNull TokenList tokens, @NotNull ArrayList<DescriptorNode> descriptors) {
         assert tokens.tokenIs(TokenType.OPERATOR_SP);
         OperatorDefinitionNode op_sp = OperatorDefinitionNode.parse(tokens);
         op_sp.addDescriptor(descriptors.toArray(new DescriptorNode[0]));
         return op_sp;
     }
 
-    private static ClassStatementNode parseDescriptorVar(TokenList tokens, ArrayList<DescriptorNode> descriptors) {
+    /**
+     * Parses a name preceded by a descriptor.
+     * <p>
+     *     This is a delegator to one of two functions, either {@link
+     *     DeclaredAssignmentNode#parse} or {@link
+     *     DeclarationNode#parse}, depending on whether or not there
+     *     is an assignment happening.
+     * </p>
+     * @param tokens The list of tokens to be destructively parsed
+     * @param descriptors The descriptors to be added onto the end
+     * @return The statement with added descriptors
+     */
+    @NotNull
+    private static ClassStatementNode parseDescriptorVar(@NotNull TokenList tokens, @NotNull ArrayList<DescriptorNode> descriptors) {
         assert tokens.tokenIs(TokenType.NAME);
         ClassStatementNode stmt;
         if (tokens.lineContains(TokenType.ASSIGN)) {
