4b01c40e7dfcc5a9098a5b5dee4615cf95c1ee27
Patrick Norton <patrick.147.norton@gmail.com>
2019-08-31 10:03:38 -0400

    Added documentation, again

--- src/ForStatementNode.java
+++ src/ForStatementNode.java
@@ -1,11 +1,24 @@
-import java.util.Optional;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
 
+/**
+ * The class representing a for-statement.
+ * @author Patrick Norton
+ */
 public class ForStatementNode implements FlowStatementNode {
     private TypedVariableNode[] vars;
     private TestNode[] iterables;
     private StatementBodyNode body;
     private StatementBodyNode nobreak;
 
+    /**
+     * Construct a new instance of ForStatementNode.
+     * @param vars The variables iterating on each loop
+     * @param iterables The iterables being iterated over
+     * @param body The body of the loop
+     * @param nobreak The nobreak statement body
+     */
+    @Contract(pure = true)
     public ForStatementNode(TypedVariableNode[] vars, TestNode[] iterables, StatementBodyNode body, StatementBodyNode nobreak) {
         this.vars = vars;
         this.iterables = iterables;
@@ -30,7 +43,21 @@ public class ForStatementNode implements FlowStatementNode {
         return nobreak;
     }
 
-    static ForStatementNode parse(TokenList tokens) {
+    /**
+     * Parse a ForStatementNode from a list of tokens.
+     * <p>
+     *     The syntax of a for loop is: <code>"for" {@link TypedVariableNode}
+     *     *("," {@link TypedVariableNode}) [","] "in" {@link TestNode} *(","
+     *     {@link TestNode}) [","] {@link StatementBodyNode} ["nobreak" {@link
+     *     StatementBodyNode}]</code>. The list of tokens must begin with a
+     *     "for" in order to parse properly.
+     * </p>
+     * @param tokens The list of tokens to be destructively parsed
+     * @return The newly parsed ForStatementNode
+     */
+    @NotNull
+    @Contract("_ -> new")
+    static ForStatementNode parse(@NotNull TokenList tokens) {
         assert tokens.tokenIs("for");
         tokens.nextToken();
         TypedVariableNode[] vars = TypedVariableNode.parseForVars(tokens);
--- src/FunctionCallNode.java
+++ src/FunctionCallNode.java
@@ -1,7 +1,19 @@
+import org.jetbrains.annotations.Contract;
+
+/**
+ * The class representing a function call.
+ * <p>
+ *     This class is for function calls within code, not function definitions
+ *     or anything else that might look like it could be parsed using this.
+ *     This class does not have any sort of parse method, that is all handled
+ *     by {@link NameNode#parse}.
+ * </p>
+ */
 public class FunctionCallNode implements NameNode {
     private TestNode caller;
     private ArgumentNode[] parameters;
 
+    @Contract(pure = true)
     public FunctionCallNode(TestNode caller, ArgumentNode[] parameters) {
         this.caller = caller;
         this.parameters = parameters;
