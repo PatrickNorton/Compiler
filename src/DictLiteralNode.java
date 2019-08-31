ca8a5af1bb22a1c4603d155d9670a97b96e23f3c
Patrick Norton <patrick.147.norton@gmail.com>
2019-08-30 21:01:07 -0400

    Kept on documenting classes

--- src/DictLiteralNode.java
+++ src/DictLiteralNode.java
@@ -1,9 +1,24 @@
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
 import java.util.LinkedList;
 
+/**
+ * The class representing a dictionary literal.
+ * <p>
+ *     This class is separate from {@link LiteralNode} because it has a slightly
+ *     differing syntax and nodal representation. See also the difference
+ *     between {@link ComprehensionNode} and {@link DictComprehensionNode}.
+ * </p>
+ * @author Patrick Norton
+ * @see LiteralNode
+ * @see DictComprehensionNode
+ */
 public class DictLiteralNode implements SubTestNode {
     private TestNode[] keys;
     private TestNode[] values;
 
+    @Contract(pure = true)
     public DictLiteralNode(TestNode[] keys, TestNode[] values) {
         this.keys = keys;
         this.values = values;
@@ -17,7 +32,20 @@ public class DictLiteralNode implements SubTestNode {
         return values;
     }
 
-    static DictLiteralNode parse(TokenList tokens) {
+    /**
+     * Parse a DictLiteralNode from a list of tokens.
+     * <p>
+     *     The syntax of a dictionary literal is as follows: <code>"{" {@link
+     *     TestNode} ":" {@link TestNode} *("," {@link TestNode} ":" {@link
+     *     TestNode}) [","] "}"</code>. This is different from other
+     *     container literals.
+     * </p>
+     * @param tokens The list of tokens to be destructively parsed
+     * @return The newly parsed DictLiteralNode
+     */
+    @NotNull
+    @Contract("_ -> new")
+    static DictLiteralNode parse(@NotNull TokenList tokens) {
         assert tokens.tokenIs("{");
         tokens.nextToken(true);
         LinkedList<TestNode> keys = new LinkedList<>();
--- src/DotimesStatementNode.java
+++ src/DotimesStatementNode.java
@@ -1,8 +1,27 @@
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * The class representing a dotimes statement.
+ * <p>
+ *     Dotimes statements <i>may</i> take a variable number of loops iterated,
+ *     and do not have to simply take an integer literal, unlike {@link
+ *     BreakStatementNode}.
+ * </p>
+ * @author Patrick Norton
+ */
 public class DotimesStatementNode implements FlowStatementNode {
     private TestNode iterations;
     private StatementBodyNode body;
     private StatementBodyNode nobreak;
 
+    /**
+     * Create a new instance of DotimesStatementNode.
+     * @param iterations The number of iterations to be taken
+     * @param body The body of the node
+     * @param nobreak The nobreak statement
+     */
+    @Contract(pure = true)
     public DotimesStatementNode(TestNode iterations, StatementBodyNode body, StatementBodyNode nobreak) {
         this.iterations = iterations;
         this.body = body;
@@ -22,7 +41,19 @@ public class DotimesStatementNode implements FlowStatementNode {
         return nobreak;
     }
 
-    static DotimesStatementNode parse(TokenList tokens) {
+    /**
+     * Parse a DotimesStatementNode from a list of tokens.
+     * <p>
+     *     The syntax of a DotimesStatementNode is as follows: <code>"dotimes"
+     *     {@link TestNode} {@link StatementBodyNode} ["nobreak" {@link
+     *     StatementBodyNode}]</code>.
+     * </p>
+     * @param tokens
+     * @return
+     */
+    @NotNull
+    @Contract("_ -> new")
+    static DotimesStatementNode parse(@NotNull TokenList tokens) {
         assert tokens.tokenIs("dotimes");
         tokens.nextToken();
         TestNode iterations = TestNode.parse(tokens);
