6ef4d2ae7bec697992e8e09bf2634f677032a18e
Patrick Norton <patrick.147.norton@gmail.com>
2019-08-30 21:01:07 -0400

    Kept on documenting classes

--- src/DictComprehensionNode.java
+++ src/DictComprehensionNode.java
@@ -1,9 +1,33 @@
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * The class representing a dictionary comprehension.
+ * <p>
+ *     This class is intentionally separate from {@link ComprehensionNode},
+ *     because normal comprehensions do not have the key-value pairing that
+ *     a dict comprehension does. See also the difference between {@link
+ *     LiteralNode} and {@link DictLiteralNode}.
+ * </p>
+ * @author Patrick Norton
+ * @see ComprehensionNode
+ * @see DictLiteralNode
+ */
 public class DictComprehensionNode implements SubTestNode {
     private TestNode key;
     private TestNode val;
     private TypedVariableNode[] vars;
     private TestNode[] looped;
 
+    /**
+     * Create new instance of DictComprehensionNode.
+     * @param key The value which is evaluated to create the dict keys
+     * @param val The value which is evaluated to create the corresponding
+     *            value
+     * @param vars The variables which are being incremented
+     * @param looped The values being looped over
+     */
+    @Contract(pure = true)
     public DictComprehensionNode(TestNode key, TestNode val, TypedVariableNode[] vars, TestNode[] looped) {
         this.key = key;
         this.val = val;
@@ -27,7 +51,21 @@ public class DictComprehensionNode implements SubTestNode {
         return looped;
     }
 
-    static DictComprehensionNode parse(TokenList tokens) {
+    /**
+     * Parse a DictComprehensionNode from a list of tokens.
+     * <p>
+     *     The syntax for a dictionary comprehension is: <code>"{" {@link
+     *     TestNode} ":" {@link TestNode} "for" {@link TypedVariableNode}
+     *     *("," {@link TypedVariableNode}) [","] "in" {@link TestNode}
+     *     *("," {@link TestNode}) [","] "}"</code>.
+     * </p>
+     * @param tokens The list of tokens to be destructively parsed
+     * @return The newly created DictComprehensionNode
+     * @see ComprehensionNode#parse
+     */
+    @NotNull
+    @Contract("_ -> new")
+    static DictComprehensionNode parse(@NotNull TokenList tokens) {
         assert tokens.tokenIs("{");
         tokens.nextToken(true);
         TestNode key = TestNode.parse(tokens, true);
@@ -39,6 +77,7 @@ public class DictComprehensionNode implements SubTestNode {
         if (!tokens.tokenIs("for")) {
             throw new ParserException("Expected for, got "+tokens.getFirst());
         }
+        // FIXME: Tokens should be parsed with ignore_newlines
         tokens.nextToken();
         TypedVariableNode[] vars = TypedVariableNode.parseList(tokens);
         if (!tokens.tokenIs("in")) {
