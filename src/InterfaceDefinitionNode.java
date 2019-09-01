e269530210be7e74db1346a50fbf6f40317831fe
Patrick Norton <patrick.147.norton@gmail.com>
2019-09-01 09:12:25 -0400

    Even more documentation

--- src/InterfaceDefinitionNode.java
+++ src/InterfaceDefinitionNode.java
@@ -1,11 +1,26 @@
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
 import java.util.LinkedList;
 
+/**
+ * The class representing an interface definition.
+ * @author Patrick Norton
+ * @see ClassDefinitionNode
+ */
 public class InterfaceDefinitionNode implements ComplexStatementNode, ClassStatementNode {
     private TypeNode name;
     private TypeNode[] superclasses;
     private InterfaceBodyNode body;
     private DescriptorNode[] descriptors;
 
+    /**
+     * Construct a new instance of InterfaceDefinitionNode.
+     * @param name The name of the interface
+     * @param superclasses The superclasses of the interface
+     * @param body The body of the interface
+     */
+    @Contract(pure = true)
     public InterfaceDefinitionNode(TypeNode name, TypeNode[] superclasses, InterfaceBodyNode body) {
         this.name = name;
         this.superclasses = superclasses;
@@ -35,12 +50,26 @@ public class InterfaceDefinitionNode implements ComplexStatementNode, ClassState
         this.descriptors = nodes;
     }
 
-    static InterfaceDefinitionNode parse(TokenList tokens) {
+    /**
+     * Parse a new interface definition from a list of tokens.
+     * <p>
+     *     The syntax for an interface definition is: <code>*({@link
+     *     DescriptorNode} "interface" {@link TypeNode} ["from" {@link
+     *     TypeNode} *("," {@link TypeNode}) [","] {@link
+     *     InterfaceBodyNode}</code>. Descriptors are parsed separately, but
+     *     the list of tokens must begin with "interface".
+     * </p>
+     * @param tokens The list of tokens to be parsed destructively
+     * @return The freshly parsed InterfaceDefinitionNode
+     */
+    @NotNull
+    @Contract("_ -> new")
+    static InterfaceDefinitionNode parse(@NotNull TokenList tokens) {
         assert tokens.tokenIs("interface");
         tokens.nextToken();
         TypeNode name = TypeNode.parse(tokens);
         LinkedList<TypeNode> superclasses = new LinkedList<>();
-        while (tokens.tokenIs("from")) {
+        while (tokens.tokenIs("from")) {  // FIXME: Doesn't parse "from" correctly
             tokens.nextToken();
             superclasses.add(TypeNode.parse(tokens));
         }
