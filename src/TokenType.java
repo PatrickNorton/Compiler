799360157d1a8f96353eb43eb31f57929808b68e
Patrick Norton <patrick.147.norton@gmail.com>
2019-09-04 16:48:48 -0400

    Added support for is and is not operators

--- src/TokenType.java
+++ src/TokenType.java
@@ -31,7 +31,7 @@ public enum TokenType {
     // The magical arrow unicorn
     ARROW("^->"),
     // Standard, boring operators, + - **
-    OPERATOR("^(==|!=|[><]=?|([+\\-*/])\\2?|<<|>>|[&|^~%])"),
+    OPERATOR("^(==|!=|[><]=?|([+\\-*/])\\2?|<<|>>|[&|^~%]|\\bis( not)?\\b)"),
     // Assignment, and dynamic assignment (:=)
     ASSIGN("^:?="),
     // String literals, including f-strings
