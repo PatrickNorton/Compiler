f442bd075959182a865211dcadb840470dd300b5
Patrick Norton <patrick.147.norton@gmail.com>
2019-09-07 17:32:22 -0400

    Fixed bugs in switch

--- src/AtomicNode.java
+++ src/AtomicNode.java
@@ -1,7 +1,24 @@
+import org.jetbrains.annotations.NotNull;
+
 /**
  * The interface for atomics, e.g. literals, names, and operators.
  *
  * @author Patrick Norton
  */
 public interface AtomicNode extends SubTestNode {
+    @NotNull
+    static AtomicNode parseLabel(@NotNull TokenList tokens) {
+        switch (tokens.getFirst().token) {
+            case NAME:
+                return NameNode.parse(tokens);
+            case OP_FUNC:
+                return OperatorTypeNode.parse(tokens);
+            case NUMBER:
+                return NumberNode.parse(tokens);
+            case STRING:
+                return StringNode.parse(tokens);
+            default:
+                throw new ParserException("Expected valid label, got "+tokens.getFirst());
+        }
+    }
 }
--- src/CaseStatementNode.java
+++ src/CaseStatementNode.java
@@ -3,19 +3,24 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.LinkedList;
 
+/**
+ * The class representing the "case" clause in a switch statement.
+ * @author Patrick Norton
+ * @see SwitchStatementNode
+ */
 public class CaseStatementNode {
-    private NameNode label;
+    private AtomicNode label;
     private StatementBodyNode body;
     private boolean fallthrough;
 
     @Contract(pure = true)
-    public CaseStatementNode(NameNode label, StatementBodyNode body, boolean fallthrough) {
+    public CaseStatementNode(AtomicNode label, StatementBodyNode body, boolean fallthrough) {
         this.label = label;
         this.body = body;
         this.fallthrough = fallthrough;
     }
 
-    public NameNode getLabel() {
+    public AtomicNode getLabel() {
         return label;
     }
 
@@ -27,6 +32,16 @@ public class CaseStatementNode {
         return fallthrough;
     }
 
+    /**
+     * Parse a case statement from a list of tokens.
+     * <p>
+     *     This is for case statements with unknown fallthrough. It will not
+     *     error on either type of statement. For grammar, see the other parse
+     *     method. The list of tokens must begin with "case" when passed.
+     * </p>
+     * @param tokens The list of tokens to be destructively parsed
+     * @return The freshly parsed CaseStatementNode.
+     */
     @NotNull
     @Contract("_ -> new")
     public static CaseStatementNode parse(@NotNull TokenList tokens) {
@@ -34,14 +49,31 @@ public class CaseStatementNode {
         return parse(tokens, tokens.tokenIs(tokens.sizeOfVariable(1), ":"));
     }
 
+    /**
+     * Parse a case statement from a list of tokens.
+     * <p>
+     *     The syntax for a no-fallthrough case statement is: <code>"case"
+     *     {@link AtomicNode} *("," {@link AtomicNode}) [","] {@link
+     *     StatementBodyNode}</code>. The syntax for a with-fallthrough case
+     *     statement is: <code>"case" {@link AtomicNode} ":" [NEWLINE]
+     *     *({@link BaseNode} NEWLINE)</code>. The list of tokens must begin
+     *     with "case" when passed.
+     * </p>
+     * @param tokens The list of tokens to be destructively parsed
+     * @param fallthrough Whether or not the statement allows fallthrough
+     * @return The freshly parsed CaseStatementNode
+     */
     @NotNull
     @Contract("_, _ -> new")
     public static CaseStatementNode parse(@NotNull TokenList tokens, boolean fallthrough) {
         assert tokens.tokenIs("case");
         tokens.nextToken();
-        NameNode label = NameNode.parse(tokens);  // FIXME: Should be AtomicNode
+        AtomicNode label = AtomicNode.parseLabel(tokens);  // FIXME: parse list if !fallthrough
         StatementBodyNode body;
         if (fallthrough) {  // TODO: Turn into StatementBodyNode.parseSwitch
+            if (!tokens.tokenIs(":")) {
+                throw new ParserException("Expected :, got " + tokens.getFirst());
+            }
             tokens.nextToken(true);
             LinkedList<BaseNode> statements = new LinkedList<>();
             while (!tokens.tokenIs("case")) {
