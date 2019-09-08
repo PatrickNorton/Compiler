32bc536fdb6c9be7f738ae38ce619915dacc3461
Patrick Norton <patrick.147.norton@gmail.com>
2019-09-08 09:48:42 -0400

    Refactored f-string parsing

--- src/FormattedStringNode.java
+++ src/FormattedStringNode.java
@@ -1,4 +1,10 @@
 import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * The class representing a formatted string.
@@ -11,9 +17,10 @@ import org.jetbrains.annotations.Contract;
  * @author Patrick Norton
  * @see StringNode
  */
-public class FormattedStringNode implements AtomicNode {
+public class FormattedStringNode implements StringLikeNode {
     private String[] strs;
     private TestNode[] tests;
+    private StringPrefix[] prefixes;
 
     /**
      * Construct a new FormattedStringNode.
@@ -21,9 +28,14 @@ public class FormattedStringNode implements AtomicNode {
      * @param tests The non-string-literals which are interpolated
      */
     @Contract(pure = true)
-    public FormattedStringNode(String[] strs, TestNode[] tests) {  // FIXME? Add string flags
+    public FormattedStringNode(String[] strs, TestNode[] tests, char[] flags) {
         this.strs = strs;
         this.tests = tests;
+        ArrayList<StringPrefix> prefixes = new ArrayList<>();
+        for (char c : flags) {
+            prefixes.add(StringPrefix.getPrefix(c));
+        }
+        this.prefixes = prefixes.toArray(new StringPrefix[0]);
     }
 
     public String[] getStrs() {
@@ -32,5 +44,48 @@ public class FormattedStringNode implements AtomicNode {
 
     public TestNode[] getTests() {
         return tests;
+    }
+
+    public StringPrefix[] getPrefixes() {
+        return prefixes;
+    }
+
+    @NotNull
+    @Contract("_, _ -> new")
+    static FormattedStringNode parse(TokenList tokens, @NotNull String contents) {
+        String inside = contents.replaceAll("(^[refb]*\")|(?<!\\\\)\"", "");
+        String prefixes = Pattern.compile("^[refb]*").matcher(contents).group();
+        LinkedList<String> strs = new LinkedList<>();
+        LinkedList<TestNode> tests = new LinkedList<>();
+        Matcher m = Pattern.compile("(?<!\\\\)(\\{([^{}]*)}?|})").matcher(inside);
+        int index = 0;
+        int start, end = 0;
+        while (m.find()) {
+            start = m.start();
+            strs.add(inside.substring(index, start - 1));
+            StringBuilder to_test = new StringBuilder();
+            int netBraces = 0;
+            do {
+                String a = m.group();
+                to_test.append(a);
+                if (a.startsWith("{")) netBraces++;
+                if (a.endsWith("}")) netBraces--;
+                if (netBraces == 0) break;
+            } while (m.find());
+            if (netBraces > 0) {
+                throw new ParserException("Unmatched braces in " + inside);
+            }
+            end = m.end();
+            TokenList tokenList = Tokenizer.parse(to_test.substring(1, to_test.length() - 1));
+            tests.add(TestNode.parse(tokenList));
+            if (!tokenList.tokenIs(TokenType.EPSILON)) {
+                throw new ParserException("Unexpected " + tokenList.getFirst());
+            }
+            index = end + 1;
+        }
+        if (index <= inside.length()) {
+            strs.add(inside.substring(end));
+        }
+        return new FormattedStringNode(strs.toArray(new String[0]), tests.toArray(new TestNode[0]), prefixes.toCharArray());
     }
 }
