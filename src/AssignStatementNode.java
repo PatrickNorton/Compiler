0df6132ba0a97d586be1bb0d12b2ee954bd5bad9
Patrick Norton <patrick.147.norton@gmail.com>
2019-09-09 09:14:03 -0400

    Improved parsing of assign statements

--- src/AssignStatementNode.java
+++ src/AssignStatementNode.java
@@ -24,62 +24,16 @@ public interface AssignStatementNode extends SimpleStatementNode {
 
     /**
      * Parse an assignment statement from a list of tokens.
-     * <p>
-     *     This uses analysis of the code as it parses in order to determine
-     *     whether or not the assignment was a declaration and initialization,
-     *     or if it was simply assignment. This is difficult to tell at parse
-     *     time, simply because there can be only a comma difference between a
-     *     syntactically valid assignment and declared assignment.
-     * </p>
      * @param tokens The tokens to be parsed. Removes tokens which are turned
      *               into the node.
      * @return The statement that was parsed out
      */
     @NotNull
-    @Contract("_ -> new")
     static AssignStatementNode parse(@NotNull TokenList tokens) {
-        ArrayList<TypeNode> var_type = new ArrayList<>();
-        ArrayList<NameNode> vars = new ArrayList<>();
-        while (!tokens.tokenIs(TokenType.ASSIGN)) {
-            if (!tokens.getToken(tokens.sizeOfVariable()).is(TokenType.ASSIGN, TokenType.COMMA)) {
-                var_type.add(TypeNode.parse(tokens));
-                vars.add(DottedVariableNode.parseName(tokens));
-                if (tokens.tokenIs(TokenType.ASSIGN)) {
-                    break;
-                }
-                if (!tokens.tokenIs(TokenType.COMMA)) {
-                    throw new ParserException("Expected comma, got " + tokens.getFirst());
-                }
-                tokens.nextToken();
-            } else {
-                var_type.add(new TypeNode(new DottedVariableNode()));
-                vars.add(DottedVariableNode.parseName(tokens));
-                if (tokens.tokenIs(TokenType.ASSIGN)) {
-                    break;
-                }
-                if (!tokens.tokenIs(TokenType.COMMA)) {
-                    throw new ParserException("Expected comma, got " + tokens.getFirst());
-                }
-                tokens.nextToken();
-            }
-        }
-        boolean is_colon = tokens.tokenIs(":=");
-        tokens.nextToken();
-        TestNode[] assignments = TestNode.parseList(tokens, false);
-        tokens.Newline();
-        TypeNode[] type_array = var_type.toArray(new TypeNode[0]);
-        NameNode[] vars_array = vars.toArray(new NameNode[0]);
-        boolean is_declared = false;
-        for (TypeNode type : var_type) {
-            if (!type.getName().isEmpty()) {
-                is_declared = true;
-                break;
-            }
-        }
-        if (is_declared) {
-            return new DeclaredAssignmentNode(is_colon, type_array, vars_array, assignments);
+        if (tokens.tokenIs(tokens.sizeOfVariable(), TokenType.ASSIGN, TokenType.COMMA)) {
+            return AssignmentNode.parse(tokens);
         } else {
-            return new AssignmentNode(is_colon, vars_array, assignments);
+            return DeclaredAssignmentNode.parse(tokens);
         }
     }
 }
