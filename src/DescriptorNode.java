a3f625e721cbb4236617ae89de2c444ed2ec3dfc
Patrick Norton <patrick.147.norton@gmail.com>
2019-09-15 06:17:41 -0400

    Added descriptor parsing

--- src/DescriptorNode.java
+++ src/DescriptorNode.java
@@ -1,5 +1,7 @@
 import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
@@ -45,6 +47,33 @@ public enum DescriptorNode implements AtomicNode {
 
     public static DescriptorNode find(String type) {
         return values.get(type);
+    }
+
+    @NotNull
+    public static DescriptorNode parse(@NotNull TokenList tokens) {
+        assert tokens.tokenIs(TokenType.DESCRIPTOR, "class");
+        if (tokens.tokenIs("class")) {
+            if (tokens.tokenIs(1, TokenType.DESCRIPTOR)) {
+                tokens.nextToken();
+                return CLASS;
+            } else {
+                throw new RuntimeException("Keyword failure");
+            }
+        } else {
+            DescriptorNode descriptor = find(tokens.getFirst().sequence);
+            tokens.nextToken();
+            return descriptor;
+        }
+    }
+
+    @NotNull
+    public static DescriptorNode[] parseList(@NotNull TokenList tokens) {
+        ArrayList<DescriptorNode> descriptors = new ArrayList<>();
+        while (tokens.tokenIs(TokenType.DESCRIPTOR) ||
+                (tokens.tokenIs("class") && tokens.tokenIs(1, TokenType.DESCRIPTOR))) {
+            descriptors.add(parse(tokens));
+        }
+        return descriptors.toArray(new DescriptorNode[0]);
     }
 
     @Contract(pure = true)
