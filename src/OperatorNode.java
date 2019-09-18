040af0e2cf9e80406da476af9c874f050b5622c7
Patrick Norton <patrick.147.norton@gmail.com>
2019-09-18 14:18:04 -0400

    Improved parsing of left-operators

--- src/OperatorNode.java
+++ src/OperatorNode.java
@@ -47,58 +47,9 @@ public class OperatorNode implements SubTestNode {
         return operands;
     }
 
-    // FIXME: Order of operations for these vs normal operators
-    /**
-     * Parse a left-starting boolean operator from a list of tokens.
-     * <p>
-     *     The only valid left-boolean is a {@code not}, so others will throw
-     *     a {@link ParserException}. The first token in the list must be a
-     *     boolean operator.
-     * </p>
-     * @param tokens The list of tokens to be parsed destructively
-     * @param ignore_newline Whether or not to ignore newlines
-     * @return The freshly parsed operator
-     */
-    @NotNull
-    @Contract("_, _ -> new")
-    static OperatorNode parseBoolOp(@NotNull TokenList tokens, boolean ignore_newline) {
-        assert tokens.tokenIs(TokenType.BOOL_OP);
-        switch (tokens.getFirst().sequence) {
-            case "not":
-                tokens.nextToken(ignore_newline);
-                return new OperatorNode(OperatorTypeNode.BOOL_NOT, TestNode.parse(tokens));
-            case "and":
-            case "or":
-            case "xor":
-                throw new ParserException(tokens.getFirst()+" must be in between statements");
-            default:
-                throw new RuntimeException("Unknown boolean operator");
-        }
-    }
-
-    /**
-     * Parse a left-starting operator from a list of tokens.
-     * <p>
-     *     The only valid left-operators are {@code -} and {@code ~}, so others
-     *     will throw a {@link ParserException}. The first token in the list
-     *     must be an operator.
-     * </p>
-     * @param tokens The list of tokens to be parsed
-     * @param ignore_newline Whether or not to ignore newlines
-     * @return The freshly parsed operator
-     */
-    @NotNull
-    @Contract("_, _ -> new")
-    static OperatorNode parse(@NotNull TokenList tokens, boolean ignore_newline) {
-        assert tokens.tokenIs(TokenType.OPERATOR);
-        if (!tokens.tokenIs("-", "~")) {
-            throw new ParserException("- is the only unary operator");
-        }
-        Token operator = tokens.getFirst();
-        tokens.nextToken(ignore_newline);
-        TestNode next = TestNode.parse(tokens, ignore_newline);
-        OperatorTypeNode op = OperatorTypeNode.findOp(operator.sequence, OperatorTypeNode.Use.STANDARD, OperatorTypeNode.Use.UNARY);
-        return new OperatorNode(op, next);
+    @Override
+    public boolean isEmpty() {
+        return operands.length == 0;
     }
 
     @Override
