c8f8dd2949e1299866f84d89f408afd92fae9b44
Patrick Norton <patrick.147.norton@gmail.com>
2019-11-26 09:38:22 -0500

    Added file info to lineInfo class

--- src/Parser/LineInfo.java
+++ src/Parser/LineInfo.java
@@ -3,16 +3,24 @@ package Parser;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
+import java.nio.file.Path;
+
 public class LineInfo {
+    private final Path path;
     private final int lineNumber;
     private final String line;
     private final int startingPoint;
 
     @Contract(pure = true)
-    public LineInfo(int lineNumber, String line, int startingPoint) {
+    public LineInfo(Path path, int lineNumber, String line, int startingPoint) {
+        this.path = path;
         this.lineNumber = lineNumber;
         this.line = line;
         this.startingPoint = startingPoint;
+    }
+
+    public Path getPath() {
+        return path;
     }
 
     public int getLineNumber() {
@@ -27,7 +35,7 @@ public class LineInfo {
     @NotNull
     @Contract(value = " -> new", pure = true)
     public static LineInfo empty() {
-        return new LineInfo(-1, "", 0) {
+        return new LineInfo(Path.of(""), -1, "", 0) {
             @Override
             public String infoString() {
                 return "Line info not available";
