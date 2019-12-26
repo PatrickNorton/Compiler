4f46a766fb10264e12e401371ecef789762eea3b
Patrick Norton <patrick.147.norton@gmail.com>
2019-12-26 11:39:45 -0500

    Added start of bytecode

---
+++ src/main/java/converter/Bytecode.java
@@ -0,0 +1,71 @@
+package main.java.converter;
+
+import java.util.Arrays;
+
+public enum Bytecode {
+    NOP(0x0, 0),
+    LOAD_NULL(0x1, 0),
+    LOAD_CONST(0x2, 2),
+    LOAD_VALUE(0x3, 2),
+    LOAD_DOT(0x4, 2),
+    LOAD_SUBSCRIPT(0x5, 2),
+    POP_TOP(0x6, 0),
+    SWAP_2(0x7, 0),
+    SWAP_3(0x8, 0),
+    // Binary operators
+    PLUS(0x10, 0),
+    MINUS(0x11, 0),
+    TIMES(0x12, 0),
+    DIVIDE(0x13, 0),
+    MOD(0x14, 0),
+    SUBSCRIPT(0x15, 0),
+    POWER(0x16, 0),
+    L_BITSHIFT(0x17, 0),
+    R_BITSHIFT(0x18, 0),
+    BITWISE_AND(0x19, 0),
+    BITWISE_OR(0x1A, 0),
+    BITWISE_XOR(0x1B, 0),
+    COMPARE(0x1C, 0),
+    DEL_SUBSCRIPT(0x1D, 0),
+    U_MINUS(0x1E, 0),
+    BITWISE_NOT(0x1F, 0),
+    BOOL_AND(0x20, 0),
+    BOOL_OR(0x21, 0),
+    BOOL_NOT(0x22, 0),
+    IDENTICAL(0x23, 0),
+    // Jumps, etc.
+    JUMP(0x30, 4),
+    JUMP_FALSE(0x31, 4),
+    JUMP_TRUE(0x32, 4),
+    JUMP_NN(0x33, 4),
+    CALL_METHOD(0x34, 2),
+    CALL_TOS(0x35, 2),
+    TAIL_METHOD(0x36, 2),
+    TAIL_TOS(0x37, 2),
+    RETURN(0x38, 0),
+    ;
+
+    public final byte value;
+    private final int operands;
+
+    Bytecode(int value, int operands) {
+        this((byte) value, operands);
+    }
+
+    Bytecode(byte value, int operands) {
+        this.value = value;
+        this.operands = operands;
+    }
+
+    static {
+        // Ensure no two bytes are the same
+        assert Arrays.stream(values())
+                .mapToInt(b -> b.value)
+                .distinct().count() == values().length
+                : "Not all values distinct in bytecode";
+    }
+
+    public int size() {
+        return operands + 1;
+    }
+}
