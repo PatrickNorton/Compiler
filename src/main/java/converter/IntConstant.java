f78ab6c89e301a097336e894dd8292003bb0eba7
Patrick Norton <patrick.147.norton@gmail.com>
2019-12-28 08:00:34 -0500

    Added equals and hashCode overloads for IntConstant

--- src/main/java/converter/IntConstant.java
+++ src/main/java/converter/IntConstant.java
@@ -4,12 +4,26 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public final class IntConstant implements LangConstant {
     private int value;
 
     public IntConstant(int value) {
         this.value = value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        IntConstant that = (IntConstant) o;
+        return value == that.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
     }
 
     @NotNull
