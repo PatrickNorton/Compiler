489570a7b4ab9c7eff33ec40e8f3ffc8e882351a
Patrick Norton <patrick.147.norton@gmail.com>
2019-12-29 06:48:59 -0500

    Added more bytecodes

--- src/main/java/converter/Bytecode.java
+++ src/main/java/converter/Bytecode.java
@@ -38,6 +38,8 @@ public enum Bytecode {
     BOOL_OR(0x21, 0),
     BOOL_NOT(0x22, 0),
     IDENTICAL(0x23, 0),
+    INSTANCEOF(0x24, 0),
+    CALL_OP(0x24, 2+2),
     // Jumps, etc.
     JUMP(0x30, 4),
     JUMP_FALSE(0x31, 4),
