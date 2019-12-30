7424c32899674c01768d991ee638b58319e11cd8
Patrick Norton <patrick.147.norton@gmail.com>
2019-12-30 14:11:24 -0500

    Added IndexedSet

---
+++ src/main/java/util/IndexedHashSet.java
@@ -0,0 +1,108 @@
+package main.java.util;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.AbstractSet;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+
+public class IndexedHashSet<E> extends AbstractSet<E> implements IndexedSet<E> {
+    private Set<E> values;
+    private List<E> valueList;
+
+    public IndexedHashSet() {
+        this.valueList = new ArrayList<>();
+        this.values = new HashSet<>();
+    }
+
+    public IndexedHashSet(Collection<? extends E> values) {
+        this.values = new HashSet<>(values);
+        this.valueList = new ArrayList<>(values);
+    }
+
+    @NotNull
+    public Iterator<E> iterator() {
+        return valueList.iterator();
+    }
+
+    @Override
+    public int size() {
+        return values.size();
+    }
+
+    @Override
+    public E get(int index) {
+        return valueList.get(index);
+    }
+
+    @Override
+    public void set(int index, E value) {
+        valueList.set(index, value);
+        if (index < size()) {
+            values.remove(value);
+        }
+        values.add(value);
+    }
+
+    @Override
+    public E remove(int index) {
+        E val = valueList.remove(index);
+        values.remove(val);
+        return val;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        if (contains(o)) {
+            values.remove(o);
+            valueList.remove(o);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return values.contains(o);
+    }
+
+    @Override
+    public boolean add(E e) {
+        boolean modified = values.add(e);
+        if (modified) valueList.add(e);
+        return modified;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        boolean modified = false;
+        for (var value : c) {
+            if (contains(value)) {
+                remove(value);
+                modified = true;
+            }
+        }
+        return modified;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        IndexedHashSet<?> that = (IndexedHashSet<?>) o;
+        return Objects.equals(values, that.values) &&
+                Objects.equals(valueList, that.valueList);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), values, valueList);
+    }
+}
---
+++ src/main/java/util/IndexedSet.java
@@ -0,0 +1,9 @@
+package main.java.util;
+
+import java.util.Set;
+
+public interface IndexedSet<E> extends Set<E> {
+    E get(int index);
+    void set(int index, E value);
+    E remove(int index);
+}
