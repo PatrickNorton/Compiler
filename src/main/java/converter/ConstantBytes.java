16acc02afdc8f52ed0e4a8bd345b10ad5c7036ac
Patrick Norton <patrick.147.norton@gmail.com>
2019-12-30 08:58:13 -0500

    Added more constants

--- src/main/java/converter/ConstantBytes.java
+++ src/main/java/converter/ConstantBytes.java
@@ -4,4 +4,5 @@ public enum ConstantBytes {
     STR,
     INT,
     BIGINT,
+    DECIMAL,
 }
--- src/main/java/converter/LangConstant.java
+++ src/main/java/converter/LangConstant.java
@@ -1,7 +1,38 @@
 package main.java.converter;
 
+import main.java.parser.NumberNode;
+import main.java.parser.StringNode;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
+import java.math.BigInteger;
 import java.util.List;
 
 public interface LangConstant {
     List<Byte> toBytes();
+
+    @NotNull
+    @Contract("_ -> new")
+    static LangConstant of(@NotNull StringNode node) {
+        return new StringConstant(node.getContents());
+    }
+
+    @NotNull
+    @Contract("_ -> new")
+    static LangConstant of(@NotNull NumberNode node) {
+        var value = node.getValue();
+        // If the value is an integer, e.g. 6
+        if (value.scale() == 0 || value.stripTrailingZeros().scale() == 0) {
+            var bigInt = value.toBigIntegerExact();
+            // If the integer is small enough to fit into an int, do so
+            if (bigInt.compareTo(BigInteger.valueOf(Integer.MAX_VALUE)) <= 0
+                    && bigInt.compareTo(BigInteger.valueOf(Integer.MIN_VALUE)) >= 0) {
+                return new IntConstant(bigInt.intValueExact());
+            } else {
+                return new BigintConstant(bigInt);
+            }
+        } else {
+            return new DecimalConstant(value);
+        }
+    }
 }
