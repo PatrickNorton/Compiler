4af197563685764d69af2efadc34e46841327f70
Patrick Norton <patrick.147.norton@gmail.com>
2019-12-31 11:29:14 -0500

    Added equals and hashCode to Argument

--- src/main/java/converter/Argument.java
+++ src/main/java/converter/Argument.java
@@ -1,5 +1,7 @@
 package main.java.converter;
 
+import java.util.Objects;
+
 public final class Argument {
     private final String name;
     private final TypeObject type;
@@ -15,5 +17,19 @@ public final class Argument {
 
     public TypeObject getType() {
         return type;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Argument argument = (Argument) o;
+        return Objects.equals(name, argument.name) &&
+                Objects.equals(type, argument.type);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, type);
     }
 }
