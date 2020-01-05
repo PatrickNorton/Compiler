d89198c7808ffe3bd1bc6ac195219cf1ea99874b
Patrick Norton <patrick.147.norton@gmail.com>
2020-01-04 20:33:17 -0500

    Added better implementations for Pair

--- src/main/java/util/Pair.java
+++ src/main/java/util/Pair.java
@@ -3,6 +3,8 @@ package main.java.util;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Objects;
+
 public class Pair<K, V> {
     private K key;
     private V value;
@@ -19,6 +21,28 @@ public class Pair<K, V> {
 
     public V getValue() {
         return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pair<?, ?> pair = (Pair<?, ?>) o;
+        return Objects.equals(key, pair.key) &&
+                Objects.equals(value, pair.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(key, value);
+    }
+
+    @Override
+    public String toString() {
+        return "Pair{" +
+                "key=" + key +
+                ", value=" + value +
+                '}';
     }
 
     @NotNull
