f78b01dfca5dd7612c20ae60a6383d0fce308867
Patrick Norton <patrick.147.norton@gmail.com>
2020-01-27 20:47:33 -0500

    Fixed bugs in IfConverter

--- src/main/java/converter/IfConverter.java
+++ src/main/java/converter/IfConverter.java
@@ -20,35 +20,38 @@ public final class IfConverter implements BaseConverter {
     @Override
     public List<Byte> convert(int start) {
         var bytes = new ArrayList<>(BaseConverter.bytes(start, node.getConditional(), info));
-        addBody(bytes, start, node.getBody());
+        boolean hasElse = !node.getElseStmt().isEmpty();
+        addBody(bytes, start, node.getBody(), node.getElifs().length > 0 || hasElse);
+        int elifsRemaining = node.getElifs().length;
         for (var elif : node.getElifs()) {
-            addElif(bytes, start, elif);
+            addElif(bytes, start, elif, hasElse || elifsRemaining > 0);
+            elifsRemaining--;
         }
-        if (!node.getElseStmt().isEmpty()) {
+        if (hasElse) {
             addElse(bytes, start, node.getElseStmt());
         }
         return bytes;
     }
 
-    private void addBody(@NotNull List<Byte> bytes, int start, StatementBodyNode body) {
+    private void addBody(@NotNull List<Byte> bytes, int start, StatementBodyNode body, boolean trailingJump) {
         var bodyBytes = BaseConverter.bytes(start + bytes.size() + Bytecode.JUMP_FALSE.size(), body, info);
         // Jump index
-        var jumpTarget = start + bytes.size() + Bytecode.JUMP_FALSE.size() + bodyBytes.size();
+        var jumpTarget = start + bytes.size() + Bytecode.JUMP_FALSE.size() + bodyBytes.size() + trailingJumpBytes(trailingJump);
         bytes.add(Bytecode.JUMP_FALSE.value);
         bytes.addAll(Util.intToBytes(jumpTarget));
         bytes.addAll(bodyBytes);
     }
 
-    private void addElif(@NotNull List<Byte> bytes, int start, @NotNull ElifStatementNode elif) {
+    private void addElif(@NotNull List<Byte> bytes, int start, @NotNull ElifStatementNode elif, boolean trailingJump) {
         var cond = elif.getTest();
         var body = elif.getBody();
         bytes.add(Bytecode.JUMP.value);
         var jumpTarget = bytes.size();
         bytes.addAll(Util.zeroToBytes());  // Set jump target as temp value
         bytes.addAll(BaseConverter.bytes(start + Bytecode.JUMP.size() + bytes.size(), cond, info));
-        addBody(bytes, start, body);
+        addBody(bytes, start, body, trailingJump);
         // Set jump target
-        var target = Util.intToBytes(start + bytes.size());
+        var target = Util.intToBytes(start + bytes.size() + trailingJumpBytes(trailingJump));
         for (int i = 0; i < target.size(); i++) {
             bytes.set(jumpTarget + i, target.get(i));
         }
@@ -61,5 +64,9 @@ public final class IfConverter implements BaseConverter {
         bytes.add(Bytecode.JUMP.value);
         bytes.addAll(Util.intToBytes(jumpTarget));
         bytes.addAll(bodyBytes);
+    }
+
+    private int trailingJumpBytes(boolean trailingJump) {
+        return trailingJump ? Bytecode.JUMP.size() : 0;
     }
 }
