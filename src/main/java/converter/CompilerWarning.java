eb34c2f1fbe2d44e32203b0f077e09bab23c4e6a
Patrick Norton <patrick.147.norton@gmail.com>
2020-02-26 06:30:44 -0500

    Changed warnings to print to stderr

--- src/main/java/converter/CompilerWarning.java
+++ src/main/java/converter/CompilerWarning.java
@@ -8,7 +8,7 @@ public final class CompilerWarning {
     private CompilerWarning() {}
 
     public static void warn(String message, @NotNull LineInfo info) {
-        System.out.printf("Warning - file %s, line %d: %s%n%s%n",
+        System.err.printf("Warning - file %s, line %d: %s%n%s%n",
                 info.getPath(), info.getLineNumber(), message, info.infoString());
     }
 
