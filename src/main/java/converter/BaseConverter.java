3dc429db51c784240a0636cb21fc3c361faf3cd3
Patrick Norton <patrick.147.norton@gmail.com>
2020-02-28 09:34:04 -0500

    Added typedef support

--- src/main/java/converter/BaseConverter.java
+++ src/main/java/converter/BaseConverter.java
@@ -18,6 +18,7 @@ import main.java.parser.ReturnStatementNode;
 import main.java.parser.StatementBodyNode;
 import main.java.parser.TestNode;
 import main.java.parser.TryStatementNode;
+import main.java.parser.TypedefStatementNode;
 import main.java.parser.WhileStatementNode;
 import org.jetbrains.annotations.NotNull;
 
@@ -66,6 +67,8 @@ public interface BaseConverter {
             return new BodyConverter(info, (StatementBodyNode) node);
         } else if (node instanceof TryStatementNode) {
             return new TryConverter(info, (TryStatementNode) node);
+        } else if (node instanceof TypedefStatementNode) {
+            return new TypedefConverter(info, (TypedefStatementNode) node);
         } else if (node instanceof WhileStatementNode) {
             return new WhileConverter(info, (WhileStatementNode) node);
         } else {
