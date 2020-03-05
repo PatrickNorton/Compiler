3009f9ad6cb4f8f42333469e7c36d1de2d3b9049
Patrick Norton <patrick.147.norton@gmail.com>
2020-03-05 13:24:44 -0500

    Fixed bug in indexing

--- src/main/java/converter/IndexConverter.java
+++ src/main/java/converter/IndexConverter.java
@@ -25,7 +25,7 @@ public final class IndexConverter implements TestConverter {
         if (type != null) {
             return new GenerifiedTypeTypeObject(type);
         }
-        var operator = node.getIndices()[0] instanceof SliceNode ? OpSpTypeNode.GET_ATTR : OpSpTypeNode.GET_SLICE;
+        var operator = node.getIndices()[0] instanceof SliceNode ? OpSpTypeNode.GET_SLICE : OpSpTypeNode.GET_ATTR;
         return TestConverter.returnType(node.getVar(), info, 1).operatorReturnType(operator);
     }
 
