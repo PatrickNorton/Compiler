cbd2599c535bf2d63493be1949a4bf69161103e9
Patrick Norton <patrick.147.norton@gmail.com>
2020-03-31 07:01:50 -0400

    Added 'as' clause in while loops

--- src/main/java/converter/WhileConverter.java
+++ src/main/java/converter/WhileConverter.java
@@ -1,5 +1,6 @@
 package main.java.converter;
 
+import main.java.parser.OperatorNode;
 import main.java.parser.WhileStatementNode;
 import org.jetbrains.annotations.NotNull;
 
@@ -18,22 +19,55 @@ public final class WhileConverter extends LoopConverter {
     @Override
     protected List<Byte> trueConvert(int start) {
         List<Byte> bytes = new ArrayList<>();
-        // While loop starts by jumping to condition, use the fact that a
-        // continue statement does the same
-        bytes.add(Bytecode.JUMP.value);
-        info.addContinue(start + 1);
+        boolean hasAs = !node.getAs().isEmpty();
+        info.setContinuePoint(start + bytes.size());
+        if (!hasAs) {
+            var cond = TestConverter.bytes(start + bytes.size(), node.getCond(), info, 1);
+            bytes.addAll(cond);
+        } else {
+            convertCondWithAs(bytes, start);
+        }
+        bytes.add(Bytecode.JUMP_FALSE.value);
+        int jumpLoc = bytes.size();
         bytes.addAll(Util.zeroToBytes());
+        if (hasAs) {
+            bytes.add(Bytecode.STORE.value);
+            bytes.addAll(Util.shortToBytes(info.varIndex(node.getAs().getName())));
+        }
         var body = BaseConverter.bytes(start + bytes.size(), node.getBody(), info);
         bytes.addAll(body);
-        info.setContinuePoint(start + bytes.size());
-        var cond = TestConverter.bytes(start + bytes.size(), node.getCond(), info, 1);
-        bytes.addAll(cond);
-        bytes.add(Bytecode.JUMP_TRUE.value);
-        bytes.addAll(Util.intToBytes(start + Bytecode.JUMP.size()));
+        bytes.add(Bytecode.JUMP.value);
+        info.addContinue(start + bytes.size());
+        bytes.addAll(Util.zeroToBytes());
         if (!node.getNobreak().isEmpty()) {
             var nobreak = BaseConverter.bytes(start + bytes.size(), node.getNobreak(), info);
             bytes.addAll(nobreak);
+            if (hasAs) {
+                int jumpPos = start + bytes.size() + Bytecode.JUMP.size() + Bytecode.POP_TOP.size();
+                bytes.add(Bytecode.JUMP.value);
+                bytes.addAll(Util.intToBytes(jumpPos));
+                Util.emplace(bytes, Util.intToBytes(start + bytes.size()), jumpLoc);
+                bytes.add(Bytecode.POP_TOP.value);
+            }
+        } else if (hasAs) {
+            Util.emplace(bytes, Util.intToBytes(start + bytes.size()), jumpLoc);
+            bytes.add(Bytecode.POP_TOP.value);
+        } else {
+            Util.emplace(bytes, Util.intToBytes(start + bytes.size()), jumpLoc);
         }
         return bytes;
+    }
+
+    private void convertCondWithAs(List<Byte> bytes, int start) {
+        var condition = node.getCond();
+        if (!(condition instanceof OperatorNode)) {
+            throw CompilerException.of(
+                    "Cannot use 'as' here: condition must be 'instanceof' or 'is not null'", condition
+            );
+        }
+        var pair = OperatorConverter.convertWithAs(start + bytes.size(), (OperatorNode) condition, info, 1);
+        info.addStackFrame();
+        info.addVariable(node.getAs().getName(), pair.getValue());
+        bytes.addAll(pair.getKey());
     }
 }
