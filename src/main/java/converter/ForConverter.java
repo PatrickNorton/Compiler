a7d7ea7349c79a1391001a392104b44940ebac9e
Patrick Norton <patrick.147.norton@gmail.com>
2020-08-26 16:20:16 -0400

    Improved for-loop codegen

--- src/main/java/converter/ForConverter.java
+++ src/main/java/converter/ForConverter.java
@@ -24,26 +24,23 @@ public final class ForConverter extends LoopConverter {
     @Contract(pure = true)
     @Override
     public List<Byte> trueConvert(int start) {
-        assert node.getVars().length == 1 : "'for' loops with multiple vars not yet implemented";
-        assert node.getIterables().size() == 1 : "'for' loops with multiple vars not yet implemented";
-        var valueConverter = TestConverter.of(info, node.getIterables().get(0), 1);
-        var iteratorType = getIteratorType();
-        var variable = node.getVars()[0].getVariable();
-        var iteratedName = variable.toString();
-        if (node.getVars()[0] instanceof TypedVariableNode) {
-            if (Builtins.FORBIDDEN_NAMES.contains(iteratedName)) {
-                throw CompilerException.format("Illegal name for variable '%s'", variable, iteratedName);
-            }
-            info.checkDefinition(iteratedName, node.getVars()[0]);
-            info.addVariable(iteratedName, iteratorType, node.getVars()[0]);
+        // assert node.getVars().length == 1 : "'for' loops with multiple vars not yet implemented";
+        // assert node.getIterables().size() == 1 : "'for' loops with multiple vars not yet implemented";
+        var varLen = node.getVars().length;
+        var iterLen = node.getIterables().size();
+        if (iterLen > varLen) {
+            throw CompilerException.of("For-loops may not have more iterables than variables", node);
+        } else if (iterLen == 1) {
+            return convertSingleIter(start);
+        } else {
+            return convertMultipleIter(start);
         }
-        var valueReturnedType = valueConverter.returnType()[0].operatorReturnType(OpSpTypeNode.ITER, info)[0];
-        if (!iteratorType.isSuperclass(valueReturnedType)) {
-            throw CompilerException.format(
-                    "'for'-loop iterator returns '%s', variable requires '%s'",
-                    node.getIterables().get(0), valueReturnedType.name(), iteratorType.name()
-            );
-        }
+    }
+
+    @NotNull
+    private List<Byte> convertSingleIter(int start) {
+        var retCount = node.getVars().length;
+        var valueConverter = TestConverter.of(info, node.getIterables().get(0), retCount);
         List<Byte> bytes = new ArrayList<>();
         bytes.add(Bytecode.LOAD_CONST.value);
         bytes.addAll(Util.shortToBytes(info.constIndex(Builtins.constantOf("iter"))));
@@ -54,9 +51,22 @@ public final class ForConverter extends LoopConverter {
         bytes.add(Bytecode.FOR_ITER.value);
         int jumpPos = bytes.size();
         bytes.addAll(Util.zeroToBytes());
-        bytes.addAll(Util.shortToBytes((short) 1));
-        bytes.add(Bytecode.STORE.value);
-        bytes.addAll(Util.shortToBytes(info.varIndex(iteratedName)));
+        bytes.addAll(Util.shortToBytes((short) retCount));
+        for (int i = 0; i < node.getVars().length; i++) {
+            var typedVar = node.getVars()[i];
+            var iteratorType = getIteratorType(i, valueConverter);
+            var variable = typedVar.getVariable();
+            var iteratedName = variable.toString();
+            if (typedVar instanceof TypedVariableNode) {
+                if (Builtins.FORBIDDEN_NAMES.contains(iteratedName)) {
+                    throw CompilerException.format("Illegal name for variable '%s'", variable, iteratedName);
+                }
+                info.checkDefinition(iteratedName, typedVar);
+                info.addVariable(iteratedName, iteratorType, typedVar);
+            }
+            bytes.add(Bytecode.STORE.value);
+            bytes.addAll(Util.shortToBytes(info.varIndex(iteratedName)));
+        }
         bytes.addAll(BaseConverter.bytes(start + bytes.size(), node.getBody(), info));
         bytes.add(Bytecode.JUMP.value);
         info.loopManager().addContinue(start + bytes.size());
@@ -66,16 +76,27 @@ public final class ForConverter extends LoopConverter {
         return bytes;
     }
 
-    private TypeObject getIteratorType() {
-        if (node.getVars()[0] instanceof VariableNode) {
-            return info.getType(node.getVars()[0].getVariable().getName());
+    private List<Byte> convertMultipleIter(int start) {
+        var varLen = node.getVars().length;
+        var iterLen = node.getIterables().size();
+        if (varLen != iterLen) {
+            throw CompilerException.of(
+                    "For loops with more than one iterable must have an equal number of variables and iterables",
+                    node
+            );
         }
-        var iteratorType = node.getVars()[0].getType();
+        throw CompilerException.of("Not yet implemented", node);
+    }
+
+    private TypeObject getIteratorType(int i, TestConverter valueConverter) {
+        if (node.getVars()[i] instanceof VariableNode) {
+            return info.getType(node.getVars()[i].getVariable().getName());
+        }
+        var iteratorType = node.getVars()[i].getType();
         if (iteratorType.isDecided()) {
             return info.getType(iteratorType);
         } else {
-            var valueConverter = TestConverter.of(info, node.getIterables().get(0), 1);
-            return valueConverter.returnType()[0].operatorReturnType(OpSpTypeNode.ITER, info)[0];
+            return valueConverter.returnType()[0].operatorReturnType(OpSpTypeNode.ITER, info)[i];
         }
     }
 }
