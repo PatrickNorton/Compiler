3e3cfa5e1b1415b9eebab317c0ebe1f580af1975
Patrick Norton <patrick.147.norton@gmail.com>
2020-10-16 15:03:28 -0400

    Factored out methods in IsConverter

--- src/main/java/converter/IsConverter.java
+++ src/main/java/converter/IsConverter.java
@@ -53,59 +53,70 @@ public final class IsConverter extends OperatorConverter {
         }
         switch (operands.length) {
             case 0:
-            case 1: {
-                CompilerWarning.warnf("'%s' with < 2 operands will always be %b", lineInfo, isType ? "is" : "is not", isType);
-                // Have to get side-effects
-                List<Byte> bytes = new ArrayList<>(TestConverter.bytes(start, operands[0].getArgument(), info, 1));
-                bytes.add(Bytecode.POP_TOP.value);
-                bytes.add(Bytecode.LOAD_CONST.value);
-                bytes.addAll(Util.shortToBytes(info.constIndex(LangConstant.of(isType))));
-                return bytes;
-            }
-            case 2: {
-                List<Byte> constBytes = getConstant();
-                if (constBytes != null) return constBytes;
-                List<Byte> bytes = new ArrayList<>(TestConverter.bytes(start, operands[0].getArgument(), info, 1));
-                bytes.addAll(TestConverter.bytes(start, operands[1].getArgument(), info, 1));
-                bytes.add(Bytecode.IDENTICAL.value);
-                if (!isType) {
-                    bytes.add(Bytecode.BOOL_NOT.value);
-                }
-                return bytes;
-            }
-            default: {
-                if (!isType) {
-                    throw CompilerException.format(
-                            "'is not' only works with 2 or fewer operands (got %d)",
-                            lineInfo, operands.length
-                    );
-                }
-                List<Byte> constBytes = getConstant();
-                if (constBytes != null) return constBytes;
-                // Since object identity is transitive, it's much easier if we
-                // simply compare everything to the first object given.
-                // Since nothing in life is ever simple, we have to do some
-                // shenanigans with the stack to ensure everything winds
-                // up in the right place.
-                List<Byte> bytes = new ArrayList<>();
-                bytes.add(Bytecode.LOAD_CONST.value);
-                bytes.addAll(Util.shortToBytes(info.constIndex(Builtins.TRUE)));
-                bytes.addAll(TestConverter.bytes(start, operands[0].getArgument(), info, 1));
-                for (int i = 1; i < operands.length - 1; i++) {
-                    bytes.add(Bytecode.DUP_TOP.value);
-                    bytes.addAll(TestConverter.bytes(start, operands[i].getArgument(), info, 1));
-                    bytes.add(Bytecode.IDENTICAL.value);  // Compare the values
-                    bytes.add(Bytecode.SWAP_3.value);     // Bring up the next one (below result & operands[0])
-                    bytes.add(Bytecode.BOOL_AND.value);   // 'and' them together
-                    bytes.add(Bytecode.SWAP_2.value);     // Put operands[0] back on top}
-                }
-                // Last one is special b/c cleanup...
-                bytes.addAll(TestConverter.bytes(start, operands[operands.length - 1].getArgument(), info, 1));
-                bytes.add(Bytecode.IDENTICAL.value);
-                bytes.add(Bytecode.BOOL_AND.value);
-                return bytes;
-            }
+            case 1:
+                return convert0(start);
+            case 2:
+                return convert2(start);
+            default:
+                return convertMany(start);
         }
+    }
+
+    private List<Byte> convert0(int start) {
+        assert operands.length == 0 || operands.length == 1;
+        CompilerWarning.warnf("'%s' with < 2 operands will always be %b", lineInfo, isType ? "is" : "is not", isType);
+        // Have to get side-effects
+        List<Byte> bytes = new ArrayList<>(TestConverter.bytes(start, operands[0].getArgument(), info, 1));
+        bytes.add(Bytecode.POP_TOP.value);
+        bytes.add(Bytecode.LOAD_CONST.value);
+        bytes.addAll(Util.shortToBytes(info.constIndex(LangConstant.of(isType))));
+        return bytes;
+    }
+
+    private List<Byte> convert2(int start) {
+        assert operands.length == 2;
+        List<Byte> constBytes = getConstant();
+        if (constBytes != null) return constBytes;
+        List<Byte> bytes = new ArrayList<>(TestConverter.bytes(start, operands[0].getArgument(), info, 1));
+        bytes.addAll(TestConverter.bytes(start, operands[1].getArgument(), info, 1));
+        bytes.add(Bytecode.IDENTICAL.value);
+        if (!isType) {
+            bytes.add(Bytecode.BOOL_NOT.value);
+        }
+        return bytes;
+    }
+
+    private List<Byte> convertMany(int start) {
+        if (!isType) {
+            throw CompilerException.format(
+                    "'is not' only works with 2 or fewer operands (got %d)",
+                    lineInfo, operands.length
+            );
+        }
+        List<Byte> constBytes = getConstant();
+        if (constBytes != null) return constBytes;
+        // Since object identity is transitive, it's much easier if we
+        // simply compare everything to the first object given.
+        // Since nothing in life is ever simple, we have to do some
+        // shenanigans with the stack to ensure everything winds
+        // up in the right place.
+        List<Byte> bytes = new ArrayList<>();
+        bytes.add(Bytecode.LOAD_CONST.value);
+        bytes.addAll(Util.shortToBytes(info.constIndex(Builtins.TRUE)));
+        bytes.addAll(TestConverter.bytes(start, operands[0].getArgument(), info, 1));
+        for (int i = 1; i < operands.length - 1; i++) {
+            bytes.add(Bytecode.DUP_TOP.value);
+            bytes.addAll(TestConverter.bytes(start, operands[i].getArgument(), info, 1));
+            bytes.add(Bytecode.IDENTICAL.value);  // Compare the values
+            bytes.add(Bytecode.SWAP_3.value);     // Bring up the next one (below result & operands[0])
+            bytes.add(Bytecode.BOOL_AND.value);   // 'and' them together
+            bytes.add(Bytecode.SWAP_2.value);     // Put operands[0] back on top}
+        }
+        // Last one is special b/c cleanup...
+        bytes.addAll(TestConverter.bytes(start, operands[operands.length - 1].getArgument(), info, 1));
+        bytes.add(Bytecode.IDENTICAL.value);
+        bytes.add(Bytecode.BOOL_AND.value);
+        return bytes;
     }
 
     @Nullable
