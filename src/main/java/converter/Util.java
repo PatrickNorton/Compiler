3b882ef2c208cfedc29df4406f1fc53ee192774d
Patrick Norton <patrick.147.norton@gmail.com>
2020-10-19 09:44:59 -0400

    Added docs to Util

--- src/main/java/converter/Util.java
+++ src/main/java/converter/Util.java
@@ -7,6 +7,11 @@ import org.jetbrains.annotations.Unmodifiable;
 import java.math.BigInteger;
 import java.util.List;
 
+/**
+ * Static class for int & byte-related functions.
+ *
+ * @author Patrick Norton
+ */
 public final class Util {
     public static final byte[] MAGIC_NUMBER = toByteArray(0x0ABADE66);  // A bad egg :)
     public static final String FILE_EXTENSION = ".newlang";
@@ -15,6 +20,17 @@ public final class Util {
 
     private Util() {}
 
+    /**
+     * Converts an {@code int} into a list of {@code bytes}.
+     * <p>
+     *     The returned list will always have a length equal to {@link
+     *     Integer#BYTES} (should always be 4). If {@code value} is known to be
+     *     0, use {@link #zeroToBytes()} instead.
+     * </p>
+     *
+     * @param value The int value to get the bytes of
+     * @return The list of bytes
+     */
     @NotNull
     @Contract(pure = true)
     @Unmodifiable
@@ -29,16 +45,39 @@ public final class Util {
 
     private static final List<Byte> ZERO_INT_BYTES = List.of((byte) 0, (byte) 0, (byte) 0, (byte) 0);
 
+    /**
+     * Optimized version of {@link #intToBytes(int)} for when the value is known
+     * to be 0.
+     *
+     * @return {@link #intToBytes}{@code (0)}
+     */
     static List<Byte> zeroToBytes() {
         return ZERO_INT_BYTES;
     }
 
     private static final List<Byte> ZERO_SHORT_BYTES = List.of((byte) 0, (byte) 0);
 
+    /**
+     * Optimized version of {@link #shortToBytes(short)} for when the value is known
+     * to be 0.
+     *
+     * @return {@link #shortToBytes}{@code (0)}
+     */
     static List<Byte> shortZeroBytes() {
         return ZERO_SHORT_BYTES;
     }
 
+    /**
+     * Converts a {@code short} into a list of {@code bytes}.
+     * <p>
+     *     The returned list will always have a length equal to {@link
+     *     Short#BYTES} (should always be 2). If {@code value} is known to be
+     *     0, use {@link #shortZeroBytes()} instead.
+     * </p>
+     *
+     * @param value The int value to get the bytes of
+     * @return The list of bytes
+     */
     @NotNull
     @Contract(pure = true)
     @Unmodifiable
@@ -49,6 +88,17 @@ public final class Util {
         );
     }
 
+    /**
+     * Equivalent to {@link #intToBytes(int)}, but returns a {@code byte[]}
+     * instead of a {@code List<Byte>}.
+     * <p>
+     *     Because Java doesn't have good unboxing facilities, this prevents
+     *     having to iterate over a list too many times.
+     * </p>
+     *
+     * @param value The int value to get the bytes of
+     * @return The list of bytes
+     */
     @NotNull
     @Contract(value = "_ -> new", pure = true)
     static byte[] toByteArray(int value) {
@@ -62,10 +112,27 @@ public final class Util {
 
     private static final byte[] ZERO_BYTE_ARRAY = new byte[] {(byte) 0, (byte) 0, (byte) 0, (byte) 0};
 
+    /**
+     * Optimized version of {@link #toByteArray(int)} for when the value is known
+     * to be 0.
+     *
+     * @return {@link #toByteArray}{@code (0)}
+     */
     static byte[] zeroByteArray() {
         return ZERO_BYTE_ARRAY;
     }
 
+    /**
+     * Equivalent to {@link #shortToBytes(short)}, but returns a {@code byte[]}
+     * instead of a {@code List<Byte>}.
+     * <p>
+     *     Because Java doesn't have good unboxing facilities, this prevents
+     *     having to iterate over a list too many times.
+     * </p>
+     *
+     * @param value The short value to get the bytes of
+     * @return The list of bytes
+     */
     @NotNull
     @Contract(value = "_ -> new", pure = true)
     static byte[] toByteArray(short value) {
@@ -75,6 +142,12 @@ public final class Util {
         };
     }
 
+    /**
+     * Unboxes a {@code List<Byte>} into a {@code byte[]}.
+     *
+     * @param values The list of bytes to unbox
+     * @return The unboxed list
+     */
     @NotNull
     static byte[] toByteArray(@NotNull List<Byte> values) {
         var bytes = new byte[values.size()];
@@ -84,6 +157,18 @@ public final class Util {
         return bytes;
     }
 
+    /**
+     * Overwrites a list with another list, starting at the given index.
+     * <code><pre>
+     * var original = [0, 1, 2, 3, 4, 5];  // Not correct syntax, but whatever
+     * Util.emplace(original, [9, 8, 7, 6], 1);
+     * System.out.println(original);  // [0, 9, 8, 7, 6, 5]
+     * </pre></code>
+     *
+     * @param original The list to be modified
+     * @param toInsert The list to insert into the other
+     * @param start The index to start at
+     */
     static void emplace(List<Byte> original, @NotNull List<Byte> toInsert, int start) {
         for (int i = 0; i < toInsert.size(); i++) {
             original.set(start + i, toInsert.get(i));
@@ -95,10 +180,32 @@ public final class Util {
     private static final BigInteger BYTE_MAX = BigInteger.valueOf(Byte.MAX_VALUE);
     private static final BigInteger BYTE_MIN = BigInteger.valueOf(Byte.MIN_VALUE);
 
+    /**
+     * Determines if the value given can fit into an {@code int} or not.
+     * <p>
+     *     If this returns {@code true}, then {@code value.}{@link
+     *     BigInteger#intValueExact() intValueExact()} will not throw an
+     *     exception. If this returns {@code false}, it will throw.
+     * </p>
+     *
+     * @param value The value to check
+     * @return If the value will fit into an int
+     */
     static boolean fitsInInt(BigInteger value) {
         return value.compareTo(BIG_MAX) <= 0 && value.compareTo(BIG_MIN) >= 0;
     }
 
+    /**
+     * Determines if the value given can fit into a {@code byte} or not.
+     * <p>
+     *     If this returns {@code true}, then {@code value.}{@link
+     *     BigInteger#byteValueExact()} byteValueExact()} will not throw an
+     *     exception. If this returns {@code false}, it will throw.
+     * </p>
+     *
+     * @param value The value to check
+     * @return If the value will fit into a byte
+     */
     static boolean fitsInByte(BigInteger value) {
         return value.compareTo(BYTE_MAX) <= 0 && value.compareTo(BYTE_MIN) >= 0;
     }
