7bf5e3cefa167283ac305f8c2a2906195c48aabb
Patrick Norton <patrick.147.norton@gmail.com>
2021-02-03 06:31:32 -0500

    Clarified error message for compiler errors due to $deny

--- src/main/java/converter/WarningType.java
+++ src/main/java/converter/WarningType.java
@@ -1,5 +1,7 @@
 package main.java.converter;
 
+import java.util.Optional;
+
 public enum WarningType {
     NO_TYPE,
     DEPRECATED,
@@ -8,4 +10,22 @@ public enum WarningType {
     UNREACHABLE,
     INFINITE_LOOP,
     ;
+
+    public Optional<String> annotationName() {
+        switch (this) {
+            case DEPRECATED:
+                return Optional.of("deprecated");
+            case UNUSED:
+                return Optional.of("unused");
+            case TRIVIAL_VALUE:
+                return Optional.of("trivial");
+            case UNREACHABLE:
+                return Optional.of("unreachable");
+            case INFINITE_LOOP:
+                return Optional.of("infinite");
+            case NO_TYPE:
+            default:
+                return Optional.empty();
+        }
+    }
 }
