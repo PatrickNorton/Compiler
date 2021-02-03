87347aa8d0066328b0be55a3e4a2db0943d7301f
Patrick Norton <patrick.147.norton@gmail.com>
2021-02-03 06:05:46 -0500

    Added error to conditional 'raise' statement

--- src/main/java/converter/RaiseConverter.java
+++ src/main/java/converter/RaiseConverter.java
@@ -22,6 +22,9 @@ public final class RaiseConverter implements TestConverter {
     @NotNull
     @Override
     public TypeObject[] returnType() {
+        if (retCount > 0 && !node.getCond().isEmpty()) {
+            throw returnError();
+        }
         var result = new TypeObject[retCount];
         Arrays.fill(result, Builtins.THROWS);
         return result;
@@ -33,6 +36,9 @@ public final class RaiseConverter implements TestConverter {
         List<Byte> bytes = new ArrayList<>();
         if (!node.getFrom().isEmpty()) {
             throw CompilerTodoError.of("'from' clauses in raise statements not supported yet", node);
+        }
+        if (retCount > 0 && !node.getCond().isEmpty()) {
+            throw returnError();
         }
         int condLoc = IfConverter.addJump(start, bytes, node.getCond(), info);
         var converter = TestConverter.of(info, node.getRaised(), 1);
@@ -57,5 +63,9 @@ public final class RaiseConverter implements TestConverter {
         var divergingInfo = node.getCond().isEmpty()
                 ? new DivergingInfo().knownReturn() : new DivergingInfo().possibleReturn();
         return Pair.of(convert(start), divergingInfo);
+    }
+
+    private CompilerException returnError() {
+        return CompilerException.of("'raise' statement with trailing 'if' does not return values", node);
     }
 }
